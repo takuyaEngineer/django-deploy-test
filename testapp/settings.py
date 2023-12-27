e4c6c34bb92194891bc9d55292b27e78954acd3b
takuyaEngineer <engineer.memorandum@gmail.com>
2023-12-27 17:55:59 +0900

    fix env db

--- testapp/settings.py
+++ testapp/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'testapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
